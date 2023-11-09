ef7a41017dc9ea5d71e44eba448cec835acd602e
Abhiram Polisetti <121142994+AbhiramPolisetti@users.noreply.github.com>
2023-11-09 18:03:54 +0530

    Update main.py

--- Patient_1/main.py
+++ Patient_1/main.py
@@ -2,11 +2,12 @@
 from prediction_utils import predict_and_save
 
 if __name__ == '__main__':
-    file_path = 's0001_re.csv'
-    input_column = 'ii'
-    sequence_length = 1000
-    target_columns = ['i', 'v1', 'v2', 'v3', 'v4', 'v5', 'v6']
-    model_folder = 'Models'
-    output_csv = 'predictions.csv'
 
-    predict_and_save(file_path, input_column, sequence_length, target_columns, model_folder, output_csv)
+  file_path = 'test_patient2_1000.csv'
+  input_column = 'ii'
+  sequence_length = 1000
+  target_columns = ['i', 'v1', 'v2', 'v3', 'v4', 'v5', 'v6']
+  model_folder = 'Models'
+  output_csv = 'predictions.csv'
+
+  predict_and_save(file_path, input_column, sequence_length, target_columns, model_folder, output_csv)
