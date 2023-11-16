1cee39437f6a9a754de848af7bde8c0e7fc8eb0b
Abhiram Polisetti <121142994+AbhiramPolisetti@users.noreply.github.com>
2023-11-16 09:28:00 +0530

    Update prediction_utils.py

--- Patient_1/prediction_utils.py
+++ Patient_1/prediction_utils.py
@@ -56,16 +56,3 @@ def predict_and_save(file_path, input_column, target_columns, model_folder, scal
     predictions_df.to_csv(output_csv, index=False)
 
 
-
-
-if __name__ == '__main__':
-    file_path = r"/content/test_patient2_1000.csv"
-    input_column = 'ii'
-    target_columns = ['i', 'v1', 'v2', 'v3', 'v4', 'v5', 'v6']
-    model_folder = r"/content"
-    scaler_folder = r"/content"  # Adjust this path
-    output_csv = 'predictions.csv'
-
-
-    predict_and_save(file_path, input_column, target_columns, model_folder, scaler_folder, output_csv)
-
