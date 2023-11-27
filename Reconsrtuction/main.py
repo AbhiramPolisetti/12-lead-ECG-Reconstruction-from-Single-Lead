bc15c10e187b7ad5b4e315ce4005dc7d8ffdf971
Abhiram Polisetti <121142994+AbhiramPolisetti@users.noreply.github.com>
2023-11-27 15:13:11 +0530

    Update main.py

--- Reconsrtuction/main.py
+++ Reconsrtuction/main.py
@@ -1,12 +1,12 @@
-# main.py
+#main.py
 from prediction_utils import predict_and_save
 
 if __name__ == '__main__':
-    file_path = r"/content/s0020are.csv"
+    file_path = r"C:\Users\iabhi\Desktop\test.csv"
     input_column = 'ii'
     target_columns = ['i', 'v1', 'v2', 'v3', 'v4', 'v5', 'v6']
-    model_folder = r"/content"
-    scaler_folder = r"/content"
-    output_csv = 'predictions.csv'
+    model_folder = r"C:\Users\iabhi\Desktop\raw\models"
+    output_csv = r"C:\Users\iabhi\Desktop\predictions.csv"
+    sequence_length = 1000  
 
-    predict_and_save(file_path, input_column, target_columns, model_folder, scaler_folder, output_csv)
+    predict_and_save(file_path, input_column, sequence_length, target_columns, model_folder, output_csv)
