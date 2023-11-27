be95fc06ec794fce87fc7d38445256a05a17e8e7
Abhiram Polisetti <121142994+AbhiramPolisetti@users.noreply.github.com>
2023-11-27 15:17:42 +0530

    Update prediction_utils.py

--- Reconsrtuction/prediction_utils.py
+++ Reconsrtuction/prediction_utils.py
@@ -22,7 +22,6 @@ def calculate_independent_leads(i_data, ii_data):
 def predict_and_save(file_path, input_column, sequence_length, target_columns, model_folder, output_csv):
     input_data = pd.read_csv(file_path)[input_column].values.reshape(-1, 1)
 
-    # Prepare sequences
     input_sequences = []
     for i in range(len(input_data) - sequence_length):
         input_sequences.append(input_data[i:i + sequence_length])
@@ -36,7 +35,6 @@ def predict_and_save(file_path, input_column, sequence_length, target_columns, m
         predicted_values, target_name = load(model_folder, target_column, input_sequences)
         predictions_df[target_name] = predicted_values.flatten()
 
-    # If additional leads are needed
     if 'i' in target_columns:
         i_data = predictions_df['i'].values
         ii_data = predictions_df[input_column].values
